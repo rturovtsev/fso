9a58cc2761526b6f35ac071c0c320b95415c6a5f
sugrob69 <sugrob.boid@gmail.com>
2016-09-02 12:22:48 +0300

    add indicators page

--- src/blocks/indicators_page_tabs/data.js
+++ src/blocks/indicators_page_tabs/data.js
@@ -1,23 +1,30 @@
 indicators_tabs: [
     {
-        title: "Паспорт"
+        title: "Паспорт",
+        href: "indicators.html"
     },
     {
-        title: "Целевые значения"
+        title: "Целевые значения",
+        href: "indicators2.html"
     },
     {
-        title: "Критические значения"
+        title: "Критические значения",
+        href: "indicators3.html"
     },
     {
-        title: "Аналитика"
+        title: "Аналитика",
+        href: "indicators4.html"
     },
     {
-        title: "Данные"
+        title: "Данные",
+        href: "indicators5.html"
     },
     {
-        title: "Графики"
+        title: "Графики",
+        href: "indicators6.html"
     },
     {
-        title: "Загрузка"
+        title: "Загрузка",
+        href: "indicators7.html"
     }
 ]
