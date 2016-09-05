1b2bbdbe2e3573c8ae42fc299f3198cf630e8e0d
sugrob69 <sugrob.boid@gmail.com>
2016-09-05 18:48:48 +0300

    fix analytic page

--- tmp/modulesData.js
+++ tmp/modulesData.js
@@ -1,3 +1,38 @@
+analyt_tabs: [
+    {
+        title: "<span class='sp_anw'>Оборона</span><span class='sp_anw'>страны</span>",
+        href: "#"
+    },{
+        title: "<span class='sp_anw'>Государственная и</span><span class='sp_anw'>общественная...</span>",
+        href: "#"
+    },{
+        title: "<span class='sp_anw'>Повышение</span><span class='sp_anw'>качества жизни</span>",
+        href: "#"
+    },{
+        title: "<span class='sp_anw'>Экономический</span><span class='sp_anw'>рост</span>",
+        href: "#"
+    },{
+        title: "<span class='sp_anw'>Наука, технологии,</span><span class='sp_anw'>образование</span>",
+        href: "#"
+    },{
+        title: "<span class='sp_anw'>Здравоохранение</span>",
+        href: "#"
+    },{
+        title: "<span class='sp_anw'>Культура</span>",
+        href: "#"
+    },{
+        title: "<span class='sp_anw'>Экология живых</span><span class='sp_anw'>систем и район...</span>",
+        href: "#"
+    },{
+        title: "<span class='sp_anw'>Стратегическая</span><span class='sp_anw'>стабильность и...</span>",
+        href: "#"
+    },{
+        title: "<span class='sp_anw'>Обеспечение</span><span class='sp_anw'>информационной...</span>",
+        href: "#"
+    },
+
+],
+
 indicators_tabs: [
     {
         title: "Паспорт",
